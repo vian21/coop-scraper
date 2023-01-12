8c3a6e022108eda500583116659b9cea3704fd85
vian21 <pigir101@uotawa.ca>
2023-01-12 13:25:34 -0500

    init

---
+++ README.md
@@ -0,0 +1,42 @@
+# COOP-Scraper
+
+### **Stack**:
+
+- NodeJS
+- [cheerio.js](https://cheerio.js.org/)
+
+## Setup:
+
+```sh
+git clone https://github.com/vian21/coop-scraper
+```
+
+```sh
+cd coop-scraper
+```
+
+```sh
+npm install
+```
+
+## Running:
+
+```sh
+    node .
+```
+
+The sccript will generate an `indEX.json` with all the data.
+
+## Adding to new data
+
+The `HTML` content was scraped using an aunthenticated account on `Jan 12th 2022`. If you want to add new data or job postings:
+
+1. Login to you indEX account
+1. Select the faculty/programmes you want to summarize
+1. Select the availablity and mobility
+1. Apply filters
+1. Right click -> View Page Source
+1. Select all the content and copy it
+1. Paste it in the `indEX.html` file
+
+All done you can now run the script again by running `node .` in the terminal.
---
+++ index.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const cherrio = require("cheerio");
+let data;
+
+try {
+  data = fs.readFileSync("indEX.html", "utf-8");
+} catch (err) {
+  console.log(err);
+}
+
+//selector
+const $ = cherrio.load(data);
+
+/***********************************
+ * Programs
+ ***********************************/
+let programs = [];
+
+$("option:selected").each((number, element) => {
+  programs.push($(element).text().trim());
+});
+//pop the last one since it contains the year of the coop
+programs.pop();
+
+/***********************************
+ * Job Postings
+ ***********************************/
+let jobPostings = [];
+
+$("div.card.col-md-5").each((number, element) => {
+  // $(element).children(".panel").children(".panel-heading").children().text()
+  const jobTitle = $(element)
+    .children(".panel")
+    .children(".panel-heading")
+    .text()
+    .replace(/\s+/g, " ")
+    .trim();
+  const jobLink = $(element)
+    .children(".panel")
+    .children(".panel-heading")
+    .children()
+    .children()
+    .attr("href");
+  jobPostings.push([jobTitle, jobLink]);
+});
+
+const summerJobs = {
+  programs: programs,
+  jobPostings: jobPostings,
+};
+
+fs.writeFileSync("indEX.json", JSON.stringify(summerJobs, null, 2));
